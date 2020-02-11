4e25378d3b7ee42bddae23d6b0e0d7fd8bcb1f80
Student <Student@PC-SE5115-16.utko.feec.vutbr.cz>
2020-02-11 11:39:55 +0100

    end

--- cv02/src/cv02/DatabaseOperation.java
+++ cv02/src/cv02/DatabaseOperation.java
@@ -4,6 +4,17 @@ import java.util.Scanner;
 
 public class DatabaseOperation {
 
+	public static void printUser(database in, float add){
+		if(in.pridejUvazek(add)){
+			System.out.print(in.getJmeno()+" "+in.getRocnik()+" "+in.getUvazek()+"\n");
+		}else{
+			System.out.print(in.getJmeno()+" "+in.getRocnik()+" "+in.getUvazek()+" Dosazen maximalni uvazek\n");
+		}
+		
+	}
+	
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int pocetOsob = 3;
@@ -26,6 +37,27 @@ public class DatabaseOperation {
 			int rocnik = sc.nextInt();
 			databazeOsob[i] = new database(jmeno, rocnik);
 			
+		}
+		while(true){
+			System.out.print("1 - 3 uzivatel\n");
+			System.out.print("4 konec\n");
+			sc = new Scanner(System.in);
+			if(sc.hasNextInt()){
+				int value = sc.nextInt();
+				if(value == 4){
+					break;
+				}else if((value > 0)&&(value < 4)){
+					System.out.print("Zadej novy uvazek: ");
+					sc = new Scanner(System.in);
+					if(sc.hasNextFloat()){
+						float add = sc.nextFloat();
+						printUser(databazeOsob[value-1], add);	
+					}else{
+						System.out.print("nezadali jste zpravne uvazek");
+					}
+					
+				}
+			}
 		}
 	}
 
